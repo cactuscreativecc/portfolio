e3cf9486601c403dc40b30fbc07a0c5bb8bf42ac
Denis Ramos <denisramos.contato@gmail.com>
2026-04-20 13:27:02 -0300

    chore: atualiza caminhos de import e novas imagens de portfólio

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { i18n } from "../i18n-config";
+import { i18n } from "@/i18n-config";
 
 export default function RootPage() {
     const router = useRouter();
