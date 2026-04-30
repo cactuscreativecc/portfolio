25e80655a10b19d0297434c4ee6c6beb9b53ddbd
Denis Ramos <denisramos.contato@gmail.com>
2026-04-30 15:26:16 -0300

    Favicon

--- app/(main)/[lang]/layout.tsx
+++ app/(main)/[lang]/layout.tsx
@@ -39,6 +39,16 @@ export async function generateMetadata({
   return {
     title,
     description,
+    icons: {
+      icon: [
+        { url: '/favicon.ico' },
+        { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+        { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      ],
+      apple: [
+        { url: '/apple-touch-icon.png' },
+      ],
+    },
     openGraph: {
       title,
       description,
